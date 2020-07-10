8544f3ded6143a668182e80e02696618ad186280
ycz <ycz@163.com>
2020-07-10 23:31:55 +0800

    开发人员提交的

--- src/main/java/com/example2/Boot6Application.java
+++ src/main/java/com/example2/Boot6Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Boot6Application {
 
 	public static void main(String[] args) {
-
+		//wojia zhushi le
 		SpringApplication.run(Boot6Application.class, args);
 	}
 }
