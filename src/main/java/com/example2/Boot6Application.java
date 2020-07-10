c3fcef23c8e467bbf9dd2888b44196e3d6d96ecf
ycz <ycz@163.com>
2020-07-10 23:05:03 +0800

    项目经理第一次提交项目boot6

---
+++ src/main/java/com/example2/Boot6Application.java
@@ -0,0 +1,13 @@
+package com.example2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot6Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Boot6Application.class, args);
+	}
+}
---
+++ src/test/java/com/example2/Boot6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
